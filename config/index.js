18accaa7a8507fc4319a4ad6dc15ea8940f62ece
SHUKORYUSOF <112839706+SHUKORYUSOF@users.noreply.github.com>
2022-09-05 12:08:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,21 +4,21 @@ export const config = {
    */
 
   // 公众号APP_ID
-  APP_ID: "",
+  APP_ID: "wx59172770a5efe3c5",
 
   // 公众号APP_SECRET
-  APP_SECRET: "",
-
+  APP_SECRET: "a0272d2bd15372c5f22f5175b1db1fb1",
+    
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
    * [
    *  {
    *    // 想要发送的人的名字
-   *    name: "老婆0",
+   *    name: "鲜鲜",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
+   *    id: "onKSs6Sz-YG9gIoZcZ3j8SmoTlx4",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    useTemplateId: "WaNaKBktE71kD9DTywA6XzIDbduHkKprBatJDpbL7D4",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
@@ -27,15 +27,15 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "鲜鲜",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "onKSs6Sz-YG9gIoZcZ3j8SmoTlx4",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "WaNaKBktE71kD9DTywA6XzIDbduHkKprBatJDpbL7D4",
       // 所在省份
-      province: "广东",
+      province: "青海",
       // 所在城市
-      city: "肇庆",
+      city: "西宁",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
@@ -102,10 +102,10 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "老婆", "year": "1997", "date": "12-18"},
+    {"type": "节日", "name": "结婚纪念日", "year": "2022", "date": "08-07"},
+    {"type": "生日", "name": "李四", "year": "1997", "date": "12-18"},
+    {"type": "节日", "name": "被搭讪纪念日", "year": "2022", "date": "08-07"},
   ],
 
   /**
@@ -123,14 +123,7 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
-    // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
-    // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
-    // sakana日
-    // {"keyword": "sakana_day", date: "2022-01-06"},
-    // ... 
+    {"keyword": "love_day", date: "2022-08-07"}
   ],
 
   /** 插槽 */
